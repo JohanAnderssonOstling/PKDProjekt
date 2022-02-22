86c57065b85b92ccd2ee0f7b004fbc521c5b18ee
johan <johandost@protonmail.com>
2022-02-22 19:49:22 +0100

    Added server and client

---
+++ client.hs
@@ -0,0 +1,28 @@
+module Client where
+import System.IO
+import Network.Socket
+
+main :: IO ()
+main = do
+    connectToServer "127.0.0.1" 18000
+
+connectToServer :: String -> Int -> IO ()
+connectToServer host port = do
+    putStrLn ("Connecting to host: " ++ host ++ " on port: " ++ show(port))
+    addrInfo <- getAddrInfo Nothing (Just host) (Just $ show port)
+    let serverAddr = head addrInfo
+    socket <- socket (addrFamily serverAddr) Stream defaultProtocol --Create client socket
+    connect socket (addrAddress serverAddr) --Connect socket to server
+    sendEcho socket
+
+sendEcho :: Socket -> IO ()
+sendEcho socket = do
+    handle <- socketToHandle socket ReadWriteMode
+    line <- hGetLine handle
+    putStrLn line
+    hPutStrLn handle "Hello from client"
+    line <- hGetLine handle
+    hPutStrLn handle line
+    hClose handle
+    close socket
+
---
+++ server.hs
@@ -0,0 +1,32 @@
+module Server where
+import System.IO
+import Control.Concurrent
+import Network.Socket
+
+
+main :: IO ()
+main = do
+    putStrLn "Creating socket"
+    sock <- socket AF_INET Stream 0 -- Create a socket
+    setSocketOption sock ReuseAddr 1 -- Set socket reuse on
+    bind sock (SockAddrInet 18000 0) -- Bind socket to port 10000
+    listen sock 2 -- Listen for connections
+    serverLoop sock
+
+serverLoop :: Socket -> IO ()
+serverLoop socket = do
+    putStrLn "Accepting clients"
+    connection <- accept socket
+    forkIO (handleClient connection) -- Create a new thread for the accepted client
+    serverLoop socket
+
+handleClient :: (Socket, socketAddr) -> IO ()
+handleClient (socket, _) = do
+    putStrLn "Client connected"
+    handle <- socketToHandle socket ReadWriteMode -- Create a sockethandle for client communication
+    hPutStrLn handle "Hello World!" -- Send a message to the client
+    line <- hGetLine handle -- Get message from client
+    putStrLn ("Message from client: " ++ line)
+    hPutStrLn handle ("Echo: " ++ line)
+    hClose handle
+    close socket
