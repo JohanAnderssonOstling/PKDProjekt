0f7bfb4cb96ffc48427e307f862125e1f6b3f36e
EmSt44 <99275246+EmSt44@users.noreply.github.com>
2022-03-01 18:46:07 +0100

    Added functionality for non-command messages

--- ParseInput.hs
+++ ParseInput.hs
@@ -11,14 +11,17 @@ import System.Exit --in case it's needed
 parseInput :: String -> IO ()
 parseInput input
  | (head input) == '/' = commands (tail input) --input is a command
- | otherwise = undefined --input is a text message
-
+ | otherwise = do                              --input is a text message
+    putStrLn input
+    newInput <- getLine
+    parseInput newInput
+ 
 
 
 commands :: String -> IO ()
-commands "quit" = undefined --leave server
+commands "quit" = undefined          --leave server
 commands "mute username" = undefined --mutes a certain user
-commands _ = undefined --unknown command error
+commands _ = undefined               --unknown command error
 
 
 
