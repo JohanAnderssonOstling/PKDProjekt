2b90bf7ca7d6bd9143cbcecd28adb1c9579afad9
EmSt44 <99275246+EmSt44@users.noreply.github.com>
2022-03-02 20:34:23 +0100

    Added Maybe type and added a string to the return values

--- ParseInput.hs
+++ ParseInput.hs
@@ -5,15 +5,15 @@ import System.IO
 import Server
 import qualified Data.Map as Map
 
-isCommand :: String -> ClientState -> ClientState
+isCommand :: String -> ClientState -> Maybe (String, ClientState)
 isCommand string clientState
- | head string == '/' = commands (words string) clientState --input is a command
- | otherwise = clientState --input is not a command
+ | head string == '/' = Just (commands (words string) clientState) --input is a command
+ | otherwise = Nothing --input is not a command
 
-commands :: [String] -> ClientState -> ClientState
-commands ["/quit"] ClientState {quit=False,muted=x} = ClientState {quit=True,muted=x} --client quits server
-commands ["/mute",username] (ClientState {quit=x,muted=mUsers}) = ClientState {quit=x, muted=(Map.insert username True mUsers)} --mutes specified username
-commands _ clientState = clientState
+commands :: [String] -> ClientState -> (String, ClientState)
+commands ["/quit"] ClientState {quit=False,muted=x} = ("Quitting current server", ClientState {quit=True,muted=x}) --client quits server
+commands ["/mute",username] (ClientState {quit=x,muted=mUsers} = ("Muted " ++ username, ClientState {quit=x, muted=(Map.insert username True mUsers)} ) --mutes specified username
+commands _ clientState = ("Unknown command",clientState)
 
 
 
