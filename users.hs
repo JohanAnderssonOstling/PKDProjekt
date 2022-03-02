54b9d751f95fd36d3705b1bf52687d369b6cc539
Isak <isak.drevstad10@gmail.com>
2022-03-02 16:32:51 +0100

    read and write multiple users to/from file

--- users.hs
+++ users.hs
@@ -1,18 +1,31 @@
-{-# LANGUAGE DeriveGeneric #-}
-import Data.Aeson
-import Data.Aeson.Text (encodeToLazyText)
-import GHC.Generics
-
+import System.IO
+import Data.Text
 
 data User = User {
       username :: String
     , password :: String
-    } deriving (Generic, Show)
+    } deriving (Show)
 
-instance ToJSON User where
-    toEncoding = genericToEncoding defaultOptions
+serializeUsers :: [User] -> String 
+serializeUsers users = Prelude.foldl getUser "" users
 
-instance FromJSON User
-    
+getUser :: String -> User -> String
+getUser acc (User username password) = acc ++ username ++ " " ++ password ++ "\n"
 
-writeUserToFile user = encodeFile "users.json" user
+writeUsers :: [User] -> IO ()
+writeUsers users = do
+    file <- openFile "users.txt" WriteMode
+    hPutStr file (serializeUsers users)
+    hClose file
+
+--------------------------
+
+userList :: [String] -> [User]
+userList [] = []
+userList (x:xs) = [User (Prelude.head (Prelude.words x)) (Prelude.last (Prelude.words x))] ++ userList xs
+
+readUsers :: IO ()
+readUsers = do 
+    content <- readFile "users.txt"
+    let users = (userList (Prelude.lines content))
+    print users
