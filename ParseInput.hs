7320d9137be704c31086b2c33249d9be68fe3742
EmSt44 <99275246+EmSt44@users.noreply.github.com>
2022-03-03 20:43:38 +0100

    added unmute

--- ParseInput.hs
+++ ParseInput.hs
@@ -10,17 +10,22 @@ data ClientState = ClientState {
     username :: Username,
     quit :: Bool,
     muted :: Map Username Bool
-}
+} deriving (Show)
+
+
 isCommand :: String -> ClientState -> Maybe (String, ClientState)
 isCommand string clientState
  | head string == '/' = Just (commands (words string) clientState) --input is a command
  | otherwise = Nothing --input is not a command
 
 commands :: [String] -> ClientState -> (String, ClientState)
-commands ["/quit"] ClientState {quit=False,muted=x} = 
-    ("disconnect", 
-    ClientState {quit=True,muted=x}) --client quits server
-commands ["/mute",username] (ClientState {quit=x,muted=mUsers}) = 
+commands ["/quit"] (ClientState {username=u,quit=False,muted=m}) = 
+    ("disconnecting", 
+    ClientState {username=u,quit=True,muted=m}) --client quits server
+commands ["/mute",username] (ClientState {username=u,quit=x,muted=mUsers}) = 
     ("Muted " ++ username, 
-    ClientState {quit=x, muted=(Map.insert username True mUsers)} ) --mutes specified username
+    ClientState {username=u, quit=x, muted=(Map.insert username True mUsers)}) --mutes specified username
+comands ["/unmute",username] (ClientState {username=u,quit=x,muted=mUsers}) =
+    ("unmuted" ++ username,
+    ClientState {username=u, quit=x, muted=(Map.insert username False mUsers)}) --unmutes specified username
 commands _ clientState = ("Unknown command",clientState)
