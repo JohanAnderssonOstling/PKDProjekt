59a8466a53d9581246d2217a4a843cea9c2a5021
Emil Strand <99275246+EmSt44@users.noreply.github.com>
2022-03-04 16:17:31 +0100

    Added Data.Set and implemented it into the functions

--- ParseInput.hs
+++ ParseInput.hs
@@ -3,18 +3,22 @@ module ParseInput where
 import Network.Socket
 import System.IO
 import Data.List
+import Data.Set (Set)
+import qualified Data.Set as Set
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Types
+type Username = String
+type UserPassword = String
 
 data ClientState = ClientState {
     username :: Username,
     quit :: Bool,
-    muted :: Map Username Bool
+    muted :: Set Username
 } deriving (Show)
 
 data ServerState = ServerState {
-   users :: [Username]
+   users :: Map Username UserPassword,
+   onlineUsers :: Set Username
 } deriving (Show)
 
 
@@ -31,27 +35,27 @@ commands ["/quit"] (ClientState {username=u,quit=False,muted=m}) serverState =
     serverState) --client quits server
 commands ["/mute",username] (ClientState {username=u,quit=q,muted=mutedUsers}) serverState = 
     ("Muted " ++ username, 
-    ClientState {username=u, quit=q, muted=(Map.insert username True mutedUsers)},
+    ClientState {username=u, quit=q, muted=Set.insert username mutedUsers},
     serverState) --mutes specified username
 commands ["/unmute",username] (ClientState {username=u,quit=q,muted=mutedUsers}) serverState =
     ("unmuted" ++ username,
-    ClientState {username=u, quit=q, muted=(Map.insert username False mutedUsers)},
+    ClientState {username=u, quit=q, muted=(Set.delete username mutedUsers)},
     serverState) --unmutes specified username
 commands ["/muted"] (ClientState {username=u,quit=q,muted=mutedUsers}) serverState
- | Map.filter (== True) mutedUsers == Map.empty = 
+ | mutedUsers == Set.empty = 
     ("No muted users", 
     ClientState {username=u,quit=q,muted=mutedUsers},
     serverState)
  | otherwise =
-    ((intercalate ", " (Map.keys (Map.filter (== True) mutedUsers))),
+    ((intercalate ", " $ Set.toList mutedUsers),
     ClientState {username=u,quit=q,muted=mutedUsers},
     serverState) --displays a list of all muted users
 commands ["/users"] clientState serverState = 
-    ((intercalate ", " $ users serverState),
+    ((intercalate ", " $ Set.toList (onlineUsers serverState)),
     clientState,
     serverState)
 commands ["/commands"] clientState serverState = ("/quit, /mute user, /unmute user, /muted, /users", clientState, serverState) --displays a list of available commands
-commands _ clientState serverState = ("Unknown command", clientState, serverState)
+commands _ clientState serverState = ("Unknown command", clientState, serverState) --unknown command
 
 
 
